d7a8091c60f4d5d364375860556929bdf5c8637a
Mauri10G <mauriguevara12@gmail.com>
2026-04-05 17:31:15 -0500

    Tic-Tac-Toe Inteligente en ejercicio 5 - Mau

---
+++ Ejercicio 5. Juego del Tic-Tac-Toe Inteligente/TicTacToeInteligente/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace TicTacToeInteligente;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Game game = new Game();
+        game.Start();
+
+        Console.WriteLine("Presiona una tecla para salir...");
+        Console.ReadKey();
+    }
+}
